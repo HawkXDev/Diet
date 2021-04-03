52bd6503064c41e649b32479c1c38e99d4441dd7
sokolserg <sergsokolkin@gmail.com>
2021-04-03 14:28:01 +0300

    Initial Commit

---
+++ Diet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Diet
+//
+//  Created by Sergey Sokolkin on 3.04.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
